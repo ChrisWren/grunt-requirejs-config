f4ff0aea09a1c837de70957e5ae767b643b5babb
artemkaint <artemkaint@nodasoft.com>
2015-04-28 23:47:10 +0300

    Implementation full function print in require config. Test fix

--- test/fixtures/fixtureOut.js
+++ test/fixtures/fixtureOut.js
@@ -1,15 +1,27 @@
 // Config added by grunt-requirejs-config
 require.config({
-  "shim": {
-    "underscore": {
-      "exports": "_"
+  shim: {
+    underscore: {
+      exports: "_"
+    },
+    foo: {
+      exports: "module",
+      deps: [
+        "bar"
+      ],
+      init: function (bar) {
+        return this.Foo.noConflict();
+      }
     }
   },
-  "paths": {
-    "jquery": "libs/jquery",
-    "underscore": "libs/underscore"
+  paths: {
+    jquery: "libs/jquery",
+    underscore: "libs/underscore",
+    foo: "libs/foo",
+    bar: "libs/bar"
   }
-});
+}
+);
 require(['appController'], function (AppController) {
   AppController();
 });
